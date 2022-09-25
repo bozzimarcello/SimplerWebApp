7bd41583770513282968bb7624b7f7fb83d22109
Marcello Bozzi <bozzimarcello@gmail.com>
2022-09-25 14:46:11 +0200

    agg. supporto patine Razor e singola pagina index con CSS

---
+++ SimplerWebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SimplerWebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
